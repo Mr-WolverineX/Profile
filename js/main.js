fc0cd54517fadcb9d93cc86b2ab6d66b3910c5b9
Mr. Wolverine <developervampire@gmail.com>
2020-09-26 19:52:25 -0500

    First commit website

---
+++ js/main.js
@@ -0,0 +1,86 @@
+//SCROLLREVEAL↓↓↓
+
+//NAVBAR
+ScrollReveal().reveal('.navbar',{
+    duration: 1000,
+    origin: 'top',
+    distance: '150px'
+
+});
+
+//HEADER-CONTENT-LEFT
+ScrollReveal().reveal('.header-content-left',{
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+
+});
+
+//HEADER-CONTENT-RIGHT
+ScrollReveal().reveal('.header-content-right',{
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+
+})
+
+//BOTTOM-HEADER-RIGTH
+ScrollReveal().reveal('.header-btn',{
+    duration: 2000,
+    //origin: 'center',
+    delay: 1000
+
+})
+
+//FRASE
+ScrollReveal().reveal('.fraseCentral',{
+    duration: 2000,
+    origin:  'left',
+    distance: '300px'
+
+})
+
+//ABOUTME-LEFT
+ScrollReveal().reveal('.about-me-lado-left',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px'
+
+})
+
+//PRICE TEXT
+ScrollReveal().reveal('.Text-Price',{
+        duration: 2000,
+        origin: 'top',
+        distance: '300px'
+
+
+})
+
+ScrollReveal().reveal('.contenedor-left-precio',{
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+
+})
+    
+ScrollReveal().reveal('.contenedor-center-precio',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px'
+
+})
+
+ScrollReveal().reveal('.contenedor-rigth-precio',{
+    duration: 2000,
+    origin: 'rigth',
+    distance: '300px'
+
+})
+
+        
+
+
+
+    
+
